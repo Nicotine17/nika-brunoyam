60f7f4bcebb5a19bea9783af8c2ae1d96d7f3693
Nika Duyshoeva <nikunchik95@gmail.com>
2022-03-02 21:50:57 +0300

    Lesson-16

---
+++ Lesson-16/index.js
@@ -0,0 +1,19 @@
+// const video = document.querySelector('.video-container video')
+// const playBtn = document.querySelector('.play-btn')
+
+// playBtn.addEventListener('click', function(event) {
+//     event.currentTarget.classList.add('hidden')
+
+//     video.play()
+// })
+
+// video.addEventListener('click', function() {
+//     playBtn.classList.remove('hidden')
+
+//     video.pause()
+// })
+
+// video.addEventListener('ended', function() {
+//     video.currentTime = 0
+//     playBtn.classList.remove('hidden')
+// })
